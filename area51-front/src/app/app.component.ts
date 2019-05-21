b895ffd33ff1efe3a882b9401a5dc629dd33b9bd
ljulia <ljulia@certant.com>
2019-05-21 14:02:17 -0300

    pedido get sales (no se si anda, no lo probe)

--- area51-front/src/app/app.component.ts
+++ area51-front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  url = 'http://localhost:8080/area51/BO/sales';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'content-type': 'application/json',
+      'accept': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    })
+  };
+
+  constructor(private http: HttpClient) {}
+
+  getSales() {
+    return this.http.get(this.url, this.httpOptions);
+  }
 }
